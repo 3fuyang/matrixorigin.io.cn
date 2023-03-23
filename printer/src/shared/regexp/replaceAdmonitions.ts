9ff7e154131c51ae9408593662036fba1821846f
3fuyang <fuyang@matrixorigin.cn>
2023-03-23 19:25:56 +0800

    ci: add MKDocs admonitions replacement tests

---
+++ printer/src/shared/regexp/replaceAdmonitions.ts
@@ -0,0 +1,22 @@
+const ADMONITIONS_PATTERN = /!!!((.(?!\n\n|\r\n\r\n))*.)/sg
+const META_PATTERN = /^\s*([^\s]+)/
+
+/**
+ * Replace MKDocs style admonitions with
+ * the [generic directives proposal](https://talk.commonmark.org/t/generic-directives-plugins-syntax/444) style.
+ * @param src Markdown content.
+ */
+export function replaceAdmonitions(src: string) {
+  return src.replace(ADMONITIONS_PATTERN, (_, p1: string) => {
+    let src = p1
+
+    const type = src.match(META_PATTERN)?.[1]
+    type && (src = src.replace(META_PATTERN, ''))
+
+    const title = src.match(META_PATTERN)?.[1]
+    title && (src = src.replace(META_PATTERN, ''))
+
+    console.log(type, title)
+    return `:::${type}[${title}]${src}\r\n:::`
+  })
+}
