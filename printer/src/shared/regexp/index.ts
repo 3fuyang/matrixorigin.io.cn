e13ad28812bcb120d1d82577b32f9c93419a25e4
fwio <624835339@qq.com>
2023-03-25 16:13:34 +0800

    feat(utils): extract markdown main heading

--- printer/src/shared/regexp/index.ts
+++ printer/src/shared/regexp/index.ts
@@ -1,1 +1,2 @@
 export * from './replaceAdmonitions'
+export * from './extractMainTitle'
--- printer/tests/regexp.test.ts
+++ printer/tests/regexp.test.ts
@@ -1,11 +1,11 @@
 import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 
-import { assert, test } from 'vitest'
+import { assert, describe, test } from 'vitest'
 
-import { replaceAdmonitions } from '../src/shared'
+import { replaceAdmonitions, extractMainTitle } from '../src/shared'
 
-test('Should replace MKDocs style admonitions with directive', async () => {
+test('Should replace MKDocs-style admonitions with generic directives', async () => {
   const [input, output] = await Promise.all([
     readFile(join(process.cwd(), './tests/input.md'), {
       encoding: 'utf8',
@@ -18,4 +18,38 @@ test('Should replace MKDocs style admonitions with directive', async () => {
   const result = replaceAdmonitions(input)
 
   assert.equal(result, output)
+})
+
+describe('Should generate proper markdown headings', () => {
+  test('matrixone-v070-发布报告', () => {
+    assert.equal(
+      extractMainTitle('# **MatrixOne v0.7.0 发布报告**\n'),
+      'matrixone-v070-发布报告'
+    )
+  })
+
+  test('matrixone-版本发布历史记录', () => {
+    assert.equal(
+      extractMainTitle('# **MatrixOne 版本发布历史记录**\n'),
+      'matrixone-版本发布历史记录'
+    )
+  })
+
+  test('matrixone-v010-release-notes', () => {
+    assert.equal(
+      extractMainTitle('# **MatrixOne v0.1.0 Release Notes**\r\n'),
+      'matrixone-v010-release-notes'
+    )
+  })
+
+  test('事务通用概念', () => {
+    assert.equal(extractMainTitle('# 事务通用概念  \n'), '事务通用概念')
+  })
+
+  test('matrixone-的显式事务', () => {
+    assert.equal(
+      extractMainTitle('# MatrixOne 的显式事务    \r\n'),
+      'matrixone-的显式事务'
+    )
+  })
 })
